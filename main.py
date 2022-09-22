35ea52d1542562d5cb5efee4c6f642d5c310b8e1
Erik Rumar <erik@valeskog.se>
2022-09-22 09:49:17 +0200

    copypasted all classroom structures

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
 
+import time
+import random
+
+
+
+
+class Class:
+    def __init__(self, class_name) -> None:
+        pass
+
+class Person:
+    def __init__(self, name, age) -> None:
+        pass
+
+class Student:
+    def __init__(self, class, course) -> None:
+        pass
+    
+class Teacher:
+    def __init__(self, course) -> None:
+        pass
+
+
+class Assignement:
+    def __init__(self) -> None:
+        pass
+    
+class Course:
+    def __init__(self) -> None:
+        pass
+
+    
+class Comment:
+    def __init__(self) -> None:
+        pass
+
+    
+class Feed:
+    def __init__(self) -> None:
+        pass
